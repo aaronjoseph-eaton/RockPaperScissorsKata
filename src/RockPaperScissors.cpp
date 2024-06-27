9e502ebc2377b21742bbd45fae6cfe87ce0bfebe
aaronjoseph-eaton <53880124+aaronjoseph-eaton@users.noreply.github.com>
2024-06-27 08:35:24 -0400

    solution 2

--- src/RockPaperScissors.cpp
+++ src/RockPaperScissors.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
+/* alternative form that uses actual enum values */
 result_t play(play_t player1, play_t player2)
 {
-    return (result_t)(((player2 << 1) + player1) % 3);
+     return ((player1 == player2) ? TIE : (((player1 + 1) % 3 == player2) ? PLAYER_2_WINS : PLAYER_1_WINS));
 }
